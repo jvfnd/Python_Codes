66fc707be975754c87c57085bb02545ac9f1e560
João Vitor <113381115+jvfnd@users.noreply.github.com>
2024-08-23 09:08:11 -0300

    Update aula2.py

--- code3/aula2.py
+++ code3/aula2.py
@@ -1,1 +1,12 @@
+from funcao import criarvetorint, somadeposicaodovetor
+
+lista = criarvetorint(8)
+
+x = int(input("Digite o valor de X: "))
+y = int(input("Digite o valor de Y: "))
+
+soma = somadeposicaodovetor(lista, x, y)
+
+print("A soma dos valores nas posições x e y é {}.".format(soma))
+
 
